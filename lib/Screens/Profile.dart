c496d22babf55cd5829774f1a2ed7d63208fdcfb
Mahi-Badaya <badayamahi11@gmail.com>
2022-07-16 23:15:17 +0530

    profile screen

--- lib/Screens/Profile.dart
+++ lib/Screens/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:printbox/Components/grids.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -31,7 +32,7 @@ class Profile extends StatelessWidget {
               height: height*0.27,
               width: width,
               decoration: BoxDecoration(
-                color: Color(0xffE8E8F5),
+                color: Color(0xffffffff),
                 borderRadius: BorderRadius.circular(20),
               ),
               child: Column(
@@ -39,17 +40,95 @@ class Profile extends StatelessWidget {
                 children: [
                   Image.asset('assets/images/profile.png'),
                   SizedBox(height: height*0.01,),
-                  Text('John Doe',style: TextStyle(color: Color(0xff22215b),fontSize: 20,fontStyle: FontStyle.italic),),
+                  Text('John Doe Chacha',style: TextStyle(color: Color(0xff22215b),fontSize: 20,fontStyle: FontStyle.italic),),
                   SizedBox(height: height*0.01,),
                   Text('Lets Get your Printout',style: TextStyle(color: Color(0xff22215b),fontSize: 15),),
                   SizedBox(height: height*0.01,),
                   Text('Select files from destination you want to upload and leave the rest on us.',textAlign:TextAlign.center,style: TextStyle(color: Color(0xff22215b).withOpacity(0.6),fontSize: 14),),
-
                 ],
               ),
             ),
+            SizedBox(height: height*0.03),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('My Folders',style: TextStyle(color: Color(0xff22215b),fontSize: 16,fontStyle: FontStyle.italic)),
+                Spacer(),
+                Icon(Icons.add,color: Color(0xff22215b),),
+                SizedBox(width: width*0.05,),
+                Icon(Icons.sort_rounded,color: Color(0xff22215b),),
+                SizedBox(width: width*0.05,),
+                Icon(Icons.arrow_forward_ios_rounded,color: Color(0xff22215b),),
+              ],
+            ),
+            SizedBox(height: height*0.02,),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomBox(height: height, width: width, colour: Colors.blue, image: 'assets/images/folder-blue.png', title: 'Minor Project',),
+                    CustomBox(height: height, width: width, colour: Colors.yellow, image: 'assets/images/folder-yellow.png', title: 'Buisness Models',),
+                  ],
+                ),
+                SizedBox(height: height*0.02,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CustomBox(height: height, width: width, colour: Colors.red, image: 'assets/images/folder-red.png', title: 'Personal Docs',),
+                    CustomBox(height: height, width: width, colour: Colors.greenAccent, image: 'assets/images/folder-green.png', title: 'Posters',),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: height*0.02,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Recents Uploads',style: TextStyle(color: Color(0xff22215b),fontSize: 16,fontStyle: FontStyle.italic)),
+                Icon(Icons.sort_rounded,color: Color(0xff22215b),),
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CustomBox extends StatelessWidget {
+  const CustomBox({Key? key, required this.height, required this.width, required this.colour, required this.image, required this.title,}) : super(key: key);
+
+  final double height;
+  final double width;
+  final Color colour;
+  final String image;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 14,vertical: 14),
+      height: height*0.14,
+      width: width*0.4,
+      decoration: BoxDecoration(
+        color: colour.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(image),
+              Icon(Icons.more_vert,color: colour,),
+            ],
+          ),
+          SizedBox(height: height*0.02,),
+          Text(title,style: TextStyle(color: colour,fontStyle: FontStyle.italic,fontSize: 16),),
+          Text('Novemeber 22,2022',style: TextStyle(color: colour,fontStyle: FontStyle.italic,fontSize: 12)),
+        ],
       ),
     );
   }
