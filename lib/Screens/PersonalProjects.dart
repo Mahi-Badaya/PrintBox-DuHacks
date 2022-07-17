e831f14c5072d9030ccc9cee84e8f111183d2db6
Mahi-Badaya <badayamahi11@gmail.com>
2022-07-17 14:16:40 +0530

    personal projects

--- lib/Screens/PersonalProjects.dart
+++ lib/Screens/PersonalProjects.dart
@@ -24,11 +24,50 @@ class PersonalProjects extends StatelessWidget {
         ),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Search(),
+          SizedBox(height: height*0.03,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Recent',style: TextStyle(color: Color(0xff22215b),fontSize: 16,fontStyle: FontStyle.italic)),
+                Icon(Icons.keyboard_arrow_down_rounded,color: Color(0xff22215b),size: 30,),
+                Spacer(),
+                Icon(Icons.sort_rounded,color: Color(0xff22215b),),
+                SizedBox(width: width*0.05,),
+                Icon(Icons.calendar_view_month_outlined,color: Color(0xff22215b),),
+              ],
+            ),
+          ),
+          SizedBox(height: height*0.04,),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: Image.asset(images[index]),
+                        title: Text(names[index],style: TextStyle(color: Color(0xff22215b),fontStyle: FontStyle.italic,fontSize: 16),),
+                        subtitle: Text('July 17,2022',style: TextStyle(color: Color(0xff22215b),fontStyle: FontStyle.italic,fontSize: 12)),
+                        trailing: Text('200 kb',style: TextStyle(color: Color(0xff22215b),fontStyle: FontStyle.italic,fontSize: 11)),
+                      ),
+                      ListTile(
+                        leading: Image.asset(images[index]),
+                        title: Text(names[index],style: TextStyle(color: Color(0xff22215b),fontStyle: FontStyle.italic,fontSize: 16),),
+                        subtitle: Text('Novemeber 22,2022',style: TextStyle(color: Color(0xff22215b),fontStyle: FontStyle.italic,fontSize: 12)),
+                        trailing: Text('320 kb',style: TextStyle(color: Color(0xff22215b),fontStyle: FontStyle.italic,fontSize: 11)),
+                      ),
+                    ],
+                  );
+                }),
+          ),
         ],
       ),
     );
   }
 }
+List<String> images = ["assets/icons/docx.png", "assets/icons/ppt.png", "assets/icons/excel.png","assets/icons/pdf.png"];
+List<String> names = ['Projects.docx', 'Projects.ppt', 'Projects.xls','Projects.pdf'];
